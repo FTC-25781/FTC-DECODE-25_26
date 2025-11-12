8eb978ea46758b07738a10764eddddc3870c44bd
Soorya Balaji <soorya.balajivijayaraghavan@gmail.com>
2025-11-11 21:07:56 -0800

    Shooter Works!!!!!!!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layered/logical2/UpdatedShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layered/logical2/UpdatedShooter.java
@@ -61,7 +61,7 @@ public class UpdatedShooter extends LinearOpMode {
             telemetry.update();
         }
     }
-    private double calculateTargetPower(double targetRPM1) { // mathematical function
+    private double calculateTargetPower(double targetRPM1) { // mathematical functions
         double x = targetRPM1;
 
         final double C4 = 8.89811E-9;

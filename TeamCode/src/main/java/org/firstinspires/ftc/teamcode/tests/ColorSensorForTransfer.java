2fc11eda43430c32ff5b202e208a7b9695484535
SS <113144462+li231sd@users.noreply.github.com>
2026-01-03 19:00:22 -0800

    Made the color sensor test work and is good for bot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ColorSensorForTransfer.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.layeredFinal.physical.colorSensorDriver.colorSensorDriver;
+
+@TeleOp(name = "Color Sensor: Normalized Logic", group = "Test")
+public class ColorSensorForTransfer extends LinearOpMode {
+
+    private colorSensorDriver color1, color2, color3;
+    private final int CLEAR_THRESHOLD = 410;
+
+    @Override
+    public void runOpMode() {
+        color1 = hardwareMap.get(colorSensorDriver.class, "color1");
+        color2 = hardwareMap.get(colorSensorDriver.class, "color2");
+        color3 = hardwareMap.get(colorSensorDriver.class, "color3");
+
+        telemetry.addLine("Initialized. Ready to start.");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            color1.update();
+            color2.update();
+            color3.update();
+
+            telemetry.addData("Kicker 1", detectColor(color1));
+            telemetry.addData("Kicker 2", detectColor(color2));
+            telemetry.addData("Kicker 3", detectColor(color3));
+
+            telemetry.addLine("--- Raw Data (Sensor 1) ---");
+            displayDebug(color1);
+
+            telemetry.update();
+        }
+    }
+
+    private String detectColor(colorSensorDriver sensor) {
+        if (sensor.rclear < 500) {
+            double greenRatio = (double) sensor.rgreen / sensor.rclear;
+            double blueRatio = (double) sensor.rblue / sensor.rclear;
+
+            if (greenRatio > (blueRatio * 1.2)) {
+                return "GREEN Ball";
+            } else {
+                return "PURPLE Ball";
+            }
+        } else {
+            return "No Ball";
+        }
+    }
+
+    private void displayDebug(colorSensorDriver sensor) {
+        telemetry.addData("Clear", sensor.rclear);
+        telemetry.addData("Red", sensor.rred);
+        telemetry.addData("Green", sensor.rgreen);
+        telemetry.addData("Blue", sensor.rblue);
+    }
+}
